07b98b905138e2fe8b96bb8ac2cc9c019592fbaf
Iskandar Souissi <skan.souissi@gmail.com>
2025-01-21 22:59:24 +0100

    Update api.jsx

--- fe/src/utils/api.jsx
+++ fe/src/utils/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://purejavaapi.snailcamp.online/test4_war/webapi/",
+    baseURL: "https://purejavaapi.snailcamp.online/test4/webapi/",
     headers: {
         "Content-Type": "application/json",
     },
